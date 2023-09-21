5089a18df695cd8ae495ab4f33277b14bf3fc1ba
ImTheGh0st <49215804+ImTheGh0st@users.noreply.github.com>
2023-09-21 23:19:47 +0100

    Improved HTTP Response Handling

--- Program.cs
+++ Program.cs
@@ -17,32 +17,28 @@ namespace GatewayPing
         /// </returns>
         static async Task Main(string[] args)
         {
-            // Create a ping test and create a statusCheck and wait for its reply. If it returns true Gateway Vienna 21 is up.
-            if (pingTest("gateway-vie-21.satowa-network.eu") && await statusCheck("https://gateway-vie-21.satowa-network.eu/"))
+            // Define the gateway information
+            var gateways = new[]
             {
-                Console.WriteLine("Gateway Vienna 21 is Up...");
-            }
-            // If Gateway Vienna 21 is not reachable or gives an unknown error back then try to switch the Gateway to Gateway il 02
-            else if (pingTest("gateway-il-02.satowa-network.eu") && await statusCheck("https://gateway-il-02.satowa-network.eu/"))
+                new { Name = "Gateway Vienna 21", Host = "gateway-vie-21.satowa-network.eu" },
+                new { Name = "Gateway Innsbruck 02", Host = "gateway-il-02.satowa-network.eu" },
+                new { Name = "Gateway Frankfurt", Host = "gateway-f-100.satowa-network.eu" }
+            };
+
+            foreach (var gateway in gateways)
             {
-                Console.WriteLine("Gateway Innsbruck 02 is Up");
-            }
-            // If Gateway IL 21 is also not up try to use Gateway Frankfurt 100
-            else if (pingTest("gateway-f-100.satowa-network.eu") && await statusCheck("https://gateway-f-100.satowa-network.eu/"))
-            {
-                Console.WriteLine("Gateway Frankfurt is Up...");
-            }
-            // If nothing is up everything is down and nothing can be routed 
-            else
-            {
-                Console.WriteLine("All hosts are down!");
+                if (pingTest(gateway.Host) && await statusCheck($"https://{gateway.Host}/"))
+                {
+                    Console.WriteLine($"{gateway.Name} is Up...");
+                    Thread.Sleep(300000); // Wait for ~5 minutes
+                    continue; // Move on to the next gateway
+                }
             }
 
-            // Wait for ~5 Minutes i guess and...
-            Thread.Sleep(300000);
-            // execute Main again
-            await Main(null);
+            // If none of the gateways are up, display a message
+            Console.WriteLine("All hosts are down!");
         }
+
         /// <summary>
         /// Get the headers from the given host, use a Secret UserAgent to bypass Bad Bots WAF Rule and return a bool. If up true, if down false
         /// </summary>
@@ -59,18 +55,14 @@ namespace GatewayPing
                 httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("");
                 // Send the Data and fetch the Response async
                 HttpResponseMessage response = await httpClient.GetAsync(host);
-                // If the response is a 2xx response then return true
-                if (response.IsSuccessStatusCode)
-                {
-                    
-                    return true;
-                }
-                // If it is something else then return false
-                else
+                // If the response is not 2xx response then return false
+                if (!response.IsSuccessStatusCode)
                 {
                     Console.Write(response);
                     return false;
                 }
+
+                return true;
             }
         }
         /// <summary>
@@ -96,16 +88,10 @@ namespace GatewayPing
             int timeout = 5000;
             // Send the Ping
             PingReply reply = pingSender.Send(host, timeout, buffer, options);
-            // If it was successfully then return true
-            if (reply.Status == IPStatus.Success)
-            {
-                return true;
-            }
-            // If not return false
-            else
-            {
-                return false;
-            }
+            // If it was not successful then return false
+            if (reply.Status != IPStatus.Success) return false;
+
+            return true;
         }
     }
 }
