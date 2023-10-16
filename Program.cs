18bdd559014c047c8654facca15ac439a212fe2f
SlushUwU <105487011+SlushUwU@users.noreply.github.com>
2023-10-16 13:32:30 +0200

    Realease 1.0.2 (#2)
    * Improved HTTP Response Handling
    
    * added directories to ignore
    
    * FIxed typo
    
    * Added Host Counter
    
    * Added mailer
    
    * Added "to" string
    
    * Updated "updateGateway"
    
    * Added Cloudflare connectivity
    
    ---------
    
    Co-authored-by: ImTheGh0st <49215804+ImTheGh0st@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -7,42 +7,41 @@ namespace GatewayPing
 {
     class Project
     {
+        
         /// <summary>
         /// Main class that includes, routes and provides all important features and connection
         /// </summary>
         /// <param name="args"></param>
         /// <returns>
         /// Up = Gateway ${Gateway} is up
-        /// Down = GAteway ${Gateway} is down
+        /// Down = Gateway ${Gateway} is down
         /// </returns>
         static async Task Main(string[] args)
         {
-            // Create a ping test and create a statusCheck and wait for its reply. If it returns true Gateway Vienna 21 is up.
-            if (pingTest("gateway-vie-21.satowa-network.eu") && await statusCheck("https://gateway-vie-21.satowa-network.eu/"))
+            
+            // Define the gateway information
+            var gateways = new[]
             {
-                Console.WriteLine("Gateway Vienna 21 is Up...");
-            }
-            // If Gateway Vienna 21 is not reachable or gives an unknown error back then try to switch the Gateway to Gateway il 02
-            else if (pingTest("gateway-il-02.satowa-network.eu") && await statusCheck("https://gateway-il-02.satowa-network.eu/"))
+                new { Name = "Gateway Vienna 21", Host = "gateway-vie-21.satowa-network.eu" },
+                new { Name = "Gateway Innsbruck 02", Host = "gateway-il-02.satowa-network.eu" },
+                new { Name = "Gateway Frankfurt", Host = "gateway-f-100.satowa-network.eu" }
+            };
+            int hostCount = 0;
+            foreach (var gateway in gateways)
             {
-                Console.WriteLine("Gateway Innsbruck 02 is Up");
-            }
-            // If Gateway IL 21 is also not up try to use Gateway Frankfurt 100
-            else if (pingTest("gateway-f-100.satowa-network.eu") && await statusCheck("https://gateway-f-100.satowa-network.eu/"))
-            {
-                Console.WriteLine("Gateway Frankfurt is Up...");
-            }
-            // If nothing is up everything is down and nothing can be routed 
-            else
-            {
-                Console.WriteLine("All hosts are down!");
+                if (pingTest(gateway.Host) && await statusCheck($"https://{gateway.Host}/"))
+                {
+                    Console.WriteLine($"{gateway.Name} is Up...");
+                    hostCount++;
+                    Thread.Sleep(300000); // Wait for ~5 minutes
+                    continue; // Move on to the next gateway
+                }
             }
 
-            // Wait for ~5 Minutes i guess and...
-            Thread.Sleep(300000);
-            // execute Main again
-            await Main(null);
+            // If none of the gateways are up, display a message
+            Console.WriteLine("All hosts are down!");
         }
+
         /// <summary>
         /// Get the headers from the given host, use a Secret UserAgent to bypass Bad Bots WAF Rule and return a bool. If up true, if down false
         /// </summary>
@@ -59,18 +58,13 @@ namespace GatewayPing
                 httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("");
                 // Send the Data and fetch the Response async
                 HttpResponseMessage response = await httpClient.GetAsync(host);
-                // If the response is a 2xx response then return true
-                if (response.IsSuccessStatusCode)
-                {
-                    
-                    return true;
-                }
-                // If it is something else then return false
-                else
+                // If the response is not 2xx response then return false
+                if (!response.IsSuccessStatusCode)
                 {
                     Console.Write(response);
-                    return false;
+                    await UpdateGateway.Update("NaN", host);
                 }
+                return true;
             }
         }
         /// <summary>
@@ -96,16 +90,10 @@ namespace GatewayPing
             int timeout = 5000;
             // Send the Ping
             PingReply reply = pingSender.Send(host, timeout, buffer, options);
-            // If it was successfully then return true
-            if (reply.Status == IPStatus.Success)
-            {
-                return true;
-            }
-            // If not return false
-            else
-            {
-                return false;
-            }
+            // If it was not successful then return false
+            if (reply.Status != IPStatus.Success) return false;
+
+            return true;
         }
     }
 }
--- updateGateway.cs
+++ updateGateway.cs
@@ -1,28 +1,77 @@
+using System;
+using System.Net.Http;
 using System.Net.Mail;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace GatewayPing
 {
-    public class updateGateway
+    public class UpdateGateway
     {
-        
-        public static void update(string currentGateway, string newGateway)
+        public static async Task Update(string currentGateway, string newGateway)
         {
-
             string from = "status@satowa-network.eu";
-            string to = "";
-
-            MailMessage message = new MailMessage(from, to);
-            message.Subject = "Down";
-            SmtpClient client = new SmtpClient("mail.satowa-network.eu");
-            client.Credentials = new System.Net.NetworkCredential("status@satowa-network.eu", "");
-
-            try
+            string to = "it@satowa-network.eu";
+            var gateways = new[]
             {
-                client.Send(message);
+                new { Name = "Gateway Vienna 21", Host = "gateway-vie-21.satowa-network.dev"},
+                new { Name = "Gateway Innsbruck 02", Host = "gateway-il-02.satowa-network.eu"},
+                new { Name = "Gateway Frankfurt", Host = "gateway-f-100.satowa-network.eu" }
+            };
+            string zoneID = "b0ac2333c3d3b486e917ef3f54bea126";
+            string dnsID = "908058f3832db6d80c2388c927fff232";
+            string apiKey = "";
+
+            if (from == gateways[0].Host)
+            {
+                // Erstellen Sie den PUT-Antrag
+                var apiUrl = $"https://api.cloudflare.com/client/v4/zones/{zoneID}/dns_records/{dnsID}";
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+                var requestData = new
+                {
+                    content = newGateway,
+                    data = new { },
+                    name = "gateway.satowa-network.eu",
+                    proxiable = true,
+                    proxied = true,
+                    ttl = 1,
+                    type = "CNAME",
+                    zone_id = zoneID,
+                    zone_name = "satowa-network.eu",
+                    tags = new string[] { }
+                };
+
+                var jsonContent = JsonSerializer.Serialize(requestData);
+                var httpContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+                var response = await client.PutAsync(apiUrl, httpContent);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Gateway erfolgreich aktualisiert.");
+                }
+                else
+                {
+                    Console.WriteLine("Fehler beim Aktualisieren des Gateways: " + response.ReasonPhrase);
+                }
             }
-            catch (Exception e)
+            else
             {
-                Console.WriteLine($"There was an issue sending the email: {e.ToString()}");
+                MailMessage message = new MailMessage(from, to);
+                message.Subject = "Down";
+                SmtpClient client = new SmtpClient("mail.satowa-network.eu");
+                client.Credentials = new System.Net.NetworkCredential("status@satowa-network.eu", "ieneSsANtaNC,16,;");
+                try
+                {
+                    client.Send(message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Beim Senden der E-Mail ist ein Fehler aufgetreten: {e.ToString()}");
+                }
             }
         }
     }
