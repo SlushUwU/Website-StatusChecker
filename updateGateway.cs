56e73040c8caa7da22913c9b80898349a23bc83c
SlushUwU <rology@satowa-network.eu>
2023-11-18 13:54:10 +0100

    Added Mailing

--- updateGateway.cs
+++ updateGateway.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Net.Mail;
 using System.Text;
@@ -22,57 +23,53 @@ namespace GatewayPing
             string zoneID = "b0ac2333c3d3b486e917ef3f54bea126";
             string dnsID = "908058f3832db6d80c2388c927fff232";
             string apiKey = "";
-
-            if (from == gateways[0].Host)
+            MailMessage message = new MailMessage(from, to);
+            message.Subject = "Down";
+            SmtpClient mailClient = new SmtpClient("mail.satowa-network.eu");
+            mailClient.Credentials = new System.Net.NetworkCredential("status@satowa-network.eu", "");
+            try
             {
-                // Erstellen Sie den PUT-Antrag
-                var apiUrl = $"https://api.cloudflare.com/client/v4/zones/{zoneID}/dns_records/{dnsID}";
-                var client = new HttpClient();
-                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+                Debug.Assert(mailClient != null, nameof(mailClient) + " != null");
+                mailClient.Send(message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Beim Senden der E-Mail ist ein Fehler aufgetreten: {e.ToString()}");
+            }
+            
+            // Erstellen Sie den PUT-Antrag
+            var apiUrl = $"https://api.cloudflare.com/client/v4/zones/{zoneID}/dns_records/{dnsID}";
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
 
-                var requestData = new
-                {
-                    content = newGateway,
-                    data = new { },
-                    name = "gateway.satowa-network.eu",
-                    proxiable = true,
-                    proxied = true,
-                    ttl = 1,
-                    type = "CNAME",
-                    zone_id = zoneID,
-                    zone_name = "satowa-network.eu",
-                    tags = new string[] { }
-                };
+            var requestData = new
+            {
+                content = newGateway,
+                data = new { },
+                name = "gateway.satowa-network.eu",
+                proxiable = true,
+                proxied = true,
+                ttl = 1,
+                type = "CNAME",
+                zone_id = zoneID,
+                zone_name = "satowa-network.eu",
+                tags = new string[] { }
+            };
 
-                var jsonContent = JsonSerializer.Serialize(requestData);
-                var httpContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+            var jsonContent = JsonSerializer.Serialize(requestData);
+            var httpContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
-                var response = await client.PutAsync(apiUrl, httpContent);
+            var response = await client.PutAsync(apiUrl, httpContent);
 
-                if (response.IsSuccessStatusCode)
-                {
-                    Console.WriteLine("Gateway erfolgreich aktualisiert.");
-                }
-                else
-                {
-                    Console.WriteLine("Fehler beim Aktualisieren des Gateways: " + response.ReasonPhrase);
-                }
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Gateway erfolgreich aktualisiert.");
             }
             else
             {
-                MailMessage message = new MailMessage(from, to);
-                message.Subject = "Down";
-                SmtpClient client = new SmtpClient("mail.satowa-network.eu");
-                client.Credentials = new System.Net.NetworkCredential("status@satowa-network.eu", "ieneSsANtaNC,16,;");
-                try
-                {
-                    client.Send(message);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"Beim Senden der E-Mail ist ein Fehler aufgetreten: {e.ToString()}");
-                }
+                Console.WriteLine("Fehler beim Aktualisieren des Gateways: " + response.ReasonPhrase);
             }
+        
         }
     }
 }
